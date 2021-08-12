379a7fd6fdbfd332b621791e6cfee136c4f4c332
Mayank <mayankmacav@gmail.com>
2021-08-12 21:52:55 +0530

    new xpath updated

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const chromium = require('chrome-aws-lambda');
-const moreBtnXPath = '/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[5]/div[2]/ytd-video-primary-info-renderer/div/div/div[3]/div/ytd-menu-renderer/yt-icon-button';
-const openTranscriptBtnXPath = '/html/body/ytd-app/ytd-popup-container/iron-dropdown[1]/div/ytd-menu-popup-renderer/paper-listbox/ytd-menu-service-item-renderer';
-const engagementPanelXPath = '/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[5]/div[2]/div/div[1]/ytd-engagement-panel-section-list-renderer/div[2]/ytd-transcript-renderer/div[2]/ytd-transcript-body-renderer';
+const moreBtnXPath = '/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[6]/div[2]/ytd-video-primary-info-renderer/div/div/div[3]/div/ytd-menu-renderer/yt-icon-button';
+const openTranscriptBtnXPath = '/html/body/ytd-app/ytd-popup-container/tp-yt-iron-dropdown[1]/div/ytd-menu-popup-renderer/tp-yt-paper-listbox/ytd-menu-service-item-renderer';
+const engagementPanelXPath = '/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[5]/div[2]/div/div[1]/ytd-engagement-panel-section-list-renderer/div[2]/ytd-transcript-renderer/div[1]/ytd-transcript-body-renderer';
 const videoURL = 'https://www.youtube.com/watch?v=';
+
+/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[6]/div[2]/ytd-video-primary-info-renderer/div/div/div[3]/div/ytd-menu-renderer/yt-icon-button/button
 
 exports.handler = async (event, context, callback) => {
   let browser;
